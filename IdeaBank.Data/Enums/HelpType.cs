4b9bb300e1f4c1df934d71cff100cb6812181617
Katerina Pouspourika <katerinapouspourika@gmail.com>
2026-02-18 15:55:04 +0200

    Add DbContext and makes changes on entities as well as enums

---
+++ IdeaBank.Data/Enums/HelpType.cs
@@ -0,0 +1,11 @@
+﻿namespace IdeaBank.Data;
+
+public enum HelpType
+{
+    Developer = 1,
+    Designer = 2,
+    Marketer = 3,
+    Investor = 4,
+    Consultant = 5,
+    Secretary = 6
+}
---
+++ IdeaBank.Data/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace IdeaBank.Data;
+
+public enum UserRole
+{
+    Creator = 1,
+    Collaborator = 2,
+    Viewer = 3 
+}
