63f12469a1bf1ae0e95ca765bd9504ed59ccde94
Katerina Pouspourika <katerinapouspourika@gmail.com>
2026-02-24 12:30:52 +0200

    Migrates a simpler idea entity

--- IdeaBank.Data/Migrations/20260224103007_SimplyfiesIdeaEntity.Designer.cs
+++ IdeaBank.Data/Migrations/20260224103007_SimplyfiesIdeaEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IdeaBank.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IdeaBank.Data.Migrations
 {
     [DbContext(typeof(IdeaBankDbContext))]
-    partial class IdeaBankDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224103007_SimplyfiesIdeaEntity")]
+    partial class SimplyfiesIdeaEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
