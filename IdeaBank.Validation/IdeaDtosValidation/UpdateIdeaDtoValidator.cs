02fde9b2b33d10b29d746bb56e4c2e36a342c505
Katerina Pouspourika <katerinapouspourika@gmail.com>
2026-02-25 17:15:14 +0200

    Adds Validation for Ideas

---
+++ IdeaBank.Validation/IdeaDtosValidation/UpdateIdeaDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using IdeaBank.Models.DTOs.Idea;
+
+namespace IdeaBank.Validation.IdeaDtosValidation;
+
+public class UpdateIdeaDtoValidator :AbstractValidator<UpdateIdeaDto>
+{
+    public UpdateIdeaDtoValidator()
+    {
+        RuleFor(i => i.Title).NotEmpty().WithMessage("Title is required");
+        RuleFor(i => i.Description).NotEmpty().WithMessage("Description is required");
+    }
+}
